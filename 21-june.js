9fafa1d48b612a6944d3f74b36fe78de70876656
bright okike <brightokike@brights-MacBook-Air.local>
2023-08-21 21:49:42 +0300

    javascript training for 21 august 2023

---
+++ 21-june.js
@@ -0,0 +1,43 @@
+// DATA TYPES
+
+// 
+
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+// The Object Datatype
+// The object data type can contain:
+
+// 1. An object
+// 2. An array
+// 3. A date
+
+let myString = "this is a string data type in programming"; // string
+let myNumber = 2348013984; // number
+let bigNumber = BigInt('3948203474324134110374037424'); // bignit 
+let myBoolean = true; // boolean
+let lastName = undefined; // undefined
+let myNull = null; // null 
+const sym2 = Symbol("fju23u2"); // symbol
+let myArr = 
+["lion", 200, {firstName: "bright"}, true, undefined, "bright", ['okike', 'okike3'] ]; // array
+let myObject = {
+    "firstKey" : 3280493482,
+    firstName: "BRIGHT",
+    lastName: "Okike",
+    completed: true,
+    items: ['oil', 'onions', 'pepper'],
+}; // object
+
+/// an object always have a key and value pair
+// {} represents an object
+
+console.log(myArr) // typeof() 
+
---
+++ main.js
@@ -0,0 +1,23 @@
+
+// console.log("hello world, i'm a javascript developer")
+
+// variables
+// variables is a container that stores a particular data and  
+// that is is pointing to particular memory space on the system. 
+
+// variable must be one word
+// camal case 
+
+// var 
+// let 
+// const 
+
+let firstName = "BRIGHT OKIKE"
+
+let myName = "BRIGHT" // VARIABLE NAMING AND INSTANTIATION
+myName = "THEO AND SHEDRACK"
+let myPhone
+ 
+myPhone = 2093840924837439
+
+console.log(firstName)
