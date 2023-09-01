b7b250ffec280191849c0aeba583216e179afb91
bright okike <brightokike@brights-MacBook-Air.local>
2023-09-01 22:11:56 +0300

    lesson on condition statement

---
+++ 01-sep.js
@@ -0,0 +1,61 @@
+
+// Operators (boolean operator that is used for evaluation and comparison)
+// >, <, >=, <=, ==, ===, !=
+
+// let a =  2 > 1 ;  // false
+// let b = ( 2 == 1 ); // false
+// let cc = ( 2 != 1 ); // false
+
+// let d = ( 'Z' > 'A' ); // true
+// // alert( 'Glow' > 'Glue' ); // false
+// // alert( 'Bee' > 'Be' ); // true
+
+
+// // 5 > 4 
+// // "apple" > "pineapple"
+// // "2" > "12"
+// let g = undefined == null // true
+// let i =  undefined === null // false
+// let l = "01" === 1 // false
+// // null == "\n0\n"
+// // null === +"\n0\n"
+
+
+// // array is a data type, that is used to store collections of data. 
+
+// // let arr = [21, 323, 323, 5454, 6454, 5653];
+// // let arrStr = ["bright", "shedrack", "theo"];
+
+
+// let myString = "bright fhajfhqlekjfh dnd hefkladfhlkd khlkn kln  fhalkfnl" 
+// +
+//  " okike" + "DJFALJFK JHL  N FDLKAF" + "FJDLFAJFAHF";
+
+// let c = 0;
+// let counter = c++; // 1
+
+//  let bright = 5 > 5; // true or false 
+
+//  console.log(d)
+
+// CONDITIONAL STATEMENT
+
+let myName = "bright okike";
+let myAlarm = 8.30;
+
+let arr = [2, 4, 5 ,6, 7, 9]
+
+if(myName === "bright"){
+    // console.log("Hello " + myName + " how are you doing today" )
+    // jd'afdjfkalf 
+    // fdlkfjkajfa
+    // debuggerhafkdl
+};
+
+// for(let i = 0; i < arr.length; i++ ) {
+//     if(arr[i] === 5){
+//         console.log("Hello " + myName + " the time is " + arr[i] + " WAKE UP!!!" )
+//     } else {
+//         console.log("the value is not 5 it's " + arr[i])
+//     }
+// }
